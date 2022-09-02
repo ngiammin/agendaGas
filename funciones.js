a83cf66898199fdb3a01a40cb8c4a10bd7cfdf64
ngiammin <apps.torneos.info@gmail.com>
2022-09-01 21:20:08 -0300

    Primeras lineas de codigo

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function doGet() {
+    return HtmlService.createTemplateFromFile(`web`).evaluate().setTitle('Agenda GAS') ;
+  }
+  
+  function obtenerDatosHTML(nombre) {
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+  } 
+  
+  function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1Ki5yYaKSqow7bDviSJTRkW5EHb1YVUH2qMpDBDhRduo').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+  }
